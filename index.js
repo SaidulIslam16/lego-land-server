98f2eb52813bf56e9f433d156fc214b76766e9a7
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-14 01:20:50 +0600

    create a post api for getting data from client side

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // Api for adding single toy entry
+        app.post('/toys', async (req, res) => {
+            const toy = req.body;
+            console.log(toy);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
